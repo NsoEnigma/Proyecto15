665135a8798d706a7a7f4eb4b8b5090f4c9cfff7
NsoEnigma <alexcelia910@gmail.com>
2021-12-12 19:02:07 +0100

    terminado

--- src/main/java/paquete/Proyecto15.java
+++ src/main/java/paquete/Proyecto15.java
@@ -2,8 +2,19 @@ package paquete;
 
 import java.util.Scanner;
 
-public class Proyecto15 {
+/**
+ *Esta clase contiene distintas variables que sirven para localizar la palabra asignada segun nuestro nombre. 
+ * @author: Alejandro Asencio
+ * @version: 12/12/2021
+ *
+ */
 
+public class Proyecto15 {
+	
+	/**
+	 * Método que genera un numero determinado de caracteres dependiendo de nuestro nombre y la localizacion del mismo
+	 * @param args El parametro args define los continuos elementos que utilizamos para localizar esta palabra
+	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
@@ -21,15 +32,18 @@ public class Proyecto15 {
 		
 		int buscaPalabra = libro.indexOf(nombreCompleto);
 		
-		System.out.println(buscaPalabra); 
-		
 		System.out.println(libro.substring(buscaPalabra,buscaPalabra+nombreCompleto.length()+2));
 		
-		System.out.println(libro.substring(nombreCompleto.length()+2, ));
+		String numeroAtras =  libro.substring(buscaPalabra+(nombreCompleto.length()+1),buscaPalabra+nombreCompleto.length()+2);
+		
+		System.out.println(numeroAtras);
+		
+		int numero = Integer.parseInt(numeroAtras);
 		
 		
+		String palabra = libro.substring(buscaPalabra+(nombreCompleto.length()+2),buscaPalabra+(nombreCompleto.length()+2)+numero);
 		
-		
+		System.out.println(palabra);	
 		
 	}
 
